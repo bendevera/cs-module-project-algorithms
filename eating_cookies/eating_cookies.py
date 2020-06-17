9e1a9acd3037f3b61e5de2f3ab85c25ab0c24c2f
bendevera <ben10devera@gmail.com>
2020-06-17 16:55:24 -0600

    saving completed assignment

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,22 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, useless_param=None):
     # Your code here
+    return eating_cookies_helper(n, 0)
 
-    pass
+def eating_cookies_helper(n, curr):
+    diff = n - curr
+    solution_count = 0
+    if diff == 0:
+        return 1
+    if diff >= 3:
+        solution_count += eating_cookies_helper(n, curr+3)
+    if diff >= 2:
+        solution_count += eating_cookies_helper(n, curr+2)
+    if diff >= 1:
+        solution_count += eating_cookies_helper(n, curr+1)
+    return solution_count
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
