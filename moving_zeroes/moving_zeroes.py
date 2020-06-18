d0649e745b9754a0b7502402b34bdf0db4f17052
bendevera <ben10devera@gmail.com>
2020-06-18 15:03:51 -0600

    saving improved implementation

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,13 +4,21 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    non_zero_spot = 0
     zero_spot = len(arr)-1
-    for i in range(len(arr)):
-        if arr[i] == 0:
-            for j in range(len(arr)-1, i, -1):
-                if arr[j] != 0:
-                    arr[j], arr[i] = arr[i], arr[j]
-                    break
+    while non_zero_spot < zero_spot:
+    # for i in range(len(arr)):
+        if arr[non_zero_spot] == 0 and arr[zero_spot] != 0:
+            arr[non_zero_spot], arr[zero_spot] = arr[zero_spot], arr[non_zero_spot]
+        elif arr[non_zero_spot] != 0:
+            non_zero_spot += 1
+        elif arr[zero_spot] == 0:
+            zero_spot -= 1
+        # if arr[i] == 0:
+        #     for j in range(len(arr)-1, i, -1):
+        #         if arr[j] != 0:
+        #             arr[j], arr[i] = arr[i], arr[j]
+        #             break
     return arr
 
 
